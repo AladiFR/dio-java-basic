cad83e5b92f8677e55d109ffe25680dcbed7010b
Matheus Luiz <matheuslr2@gmail.com>
2025-03-08 12:15:58 -0300

    class 1

---
+++ src/MyClass.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    
+// main = executável
+public static void main(String[] args) {
+
+    //tudo dentro do parênteses é um método, e todo método precisa de um parametro
+    System.out.print ("Ola mundo");   
+
+}
+
+}
