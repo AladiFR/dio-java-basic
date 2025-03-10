3d6d917339ba4364196385dd1ef29c0af318d749
Matheus Luiz <matheuslr2@gmail.com>
2025-03-10 16:22:51 -0300

    "variáveis"

---
+++ src/edu/matheus/primeirasemana/tiposvariaveis/TiposVariaveis.java
@@ -0,0 +1,21 @@
+package edu.matheus.primeirasemana.tiposvariaveis;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+    // 8 tipos primitivos: int, byte, short, long, float, double, boolean e char. 
+    // esses tipos não sao considerados objetos, porém, são valores brutos.
+
+    int idade = 23;
+    int anoFabricacao = 2022;
+    int cep = 21070333;
+    long cpf = 98765432109L; //long tem que terminar com L
+    float pi = 3.14F; //Float tem que terminar com F
+    double salarioMinimo = 1442.75; //milhar
+
+    // "final" = "const" no JAVA
+    // tem que estar em caixa alta
+
+    final double VALOR_DE_PI = 3.14;
+        
+    }
+}
