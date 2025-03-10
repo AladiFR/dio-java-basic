de67a7880c9637e68b76297eeaedd3176333c551
Matheus Luiz <matheuslr2@gmail.com>
2025-03-10 15:42:52 -0300

    "identação2"

---
+++ src/edu/matheus/primeirasemana/BoletimEstudantil.java
@@ -0,0 +1,34 @@
+package edu.matheus.primeirasemana;
+
+//sem identação:
+//public class BoletimEstudantil {
+//   public static void main(String[] args) {
+//    int mediaFinal = 2;
+//    if(mediaFinal<6)
+//    System.out.println("REPROVADO");
+//    else if(mediaFinal==6)
+//    System.out.println("PROVA FINAL");
+//    else
+//    System.out.println("APROVADO");
+//    }
+//}
+
+
+
+// identação correta:
+public class BoletimEstudantil{
+    public static void main(String[] args) {
+        int mediaFinal = 2;
+
+        if(mediaFinal < 6)
+            System.out.println("REPROVADO");
+        else if(mediaFinal == 6)
+            System.out.println("PROVA FINAL");
+        else
+            System.out.println("APROVADO");
+    }
+}
+
+
+
+
---
+++ src/edu/matheus/primeirasemana/MyClass.java
@@ -0,0 +1,24 @@
+package edu.matheus.primeirasemana;
+
+public class MyClass {
+    
+// main = executável
+public static void main(String [] args) {
+    String primeiroNome = "Matheus";
+    String segundoNome = "Luiz Rocha";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+    System.out.println(nomeCompleto);
+}
+
+//médodos:
+//TipoRetorno NomeObjetivoNoInfinitivo Parametro(s)
+
+public static String nomeCompleto (String primeiroNome, String segundoNome){
+    return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
+}
+
+}
+
+
