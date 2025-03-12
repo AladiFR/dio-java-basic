12412e4490811b9806032ac141671cdd21c1e26e
Matheus Luiz <matheuslr2@gmail.com>
2025-03-12 14:31:11 -0300

    "javadoc"

---
+++ src/edu/matheus/primeirasemana/javadoc/Calculadora.java
@@ -0,0 +1,26 @@
+package edu.matheus.primeirasemana.javadoc;
+/**
+*<h1>Calculadora</h1>
+*A Calculadora realiza opreações matemáticas entre números inteiros
+*<p>
+*<b>Note:</b> Leia atentamente a documentação destas classes 
+*para desfrutar dos recursos oferecidos pelo autor
+*
+* @autor Gleyson Sampaio
+* @version 1.0
+* @since   01/01/2022
+*/
+
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois estre é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números.
+     */
+
+     public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+    
+}
---
+++ src/edu/matheus/primeirasemana/javadoc/Comentarios.java
@@ -0,0 +1,19 @@
+package edu.matheus.primeirasemana.javadoc;
+
+public class Comentarios {
+    //Comentário de uma linha
+
+    /*
+     * multi
+     * line
+     * pode ser mais
+     * detalhado
+     */
+
+    /**
+     * é mais comum para
+     * ser utilizado
+     * para documentação 
+     * (duas estrelinhas)
+     */
+}
