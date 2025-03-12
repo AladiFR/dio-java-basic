085aa2d39c9266ff50d8552e8a12adcfacbfdf34
Matheus Luiz <matheuslr2@gmail.com>
2025-03-12 15:07:03 -0300

    "terminal"

---
+++ src/edu/matheus/primeirasemana/terminalArgumentos/MinhaClasse.java
@@ -0,0 +1,7 @@
+package edu.matheus.primeirasemana.terminalArgumentos;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Executado pelo terminal");
+    }
+}
